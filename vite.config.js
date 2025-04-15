245a9e9bdbc74a7b6f5d2922244cf6d22456ccd2
Li <shanlichin2021@fau.edu>
2025-04-14 23:22:53 -0400

    Changing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Right-to-Know-Demo/",
+  base: "/rtk-frontend/",
   plugins: [react(), tailwindcss()], 
 })
