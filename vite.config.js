d6cd8cce73e6af1417cb6addef2c8d40a993c848
Li <156018102+shanlichin2021@users.noreply.github.com>
2025-04-20 00:50:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   // tell Vite that in production all assets are served from /rtk-frontend/
   base: "/rtk-frontend/",
-
   plugins: [react(), tailwindcss()], 
-
   server: {
     proxy: {
       "/api": {
@@ -17,11 +15,5 @@ export default defineConfig({
         secure: true,
       },
     },
-  },
-
-  // (optional) you can also customize build output if you want:
-  build: {
-    outDir: "dist",
-    sourcemap: false,
   },
 });
