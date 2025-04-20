84d6bff3e508b87c9c63c7e6a31ce5c6b280673b
Li <shanlichin2021@fau.edu>
2025-04-20 00:12:27 -0400

    Changes

--- src/api.js
+++ src/api.js
@@ -1,4 +1,3 @@
-// src/api.js
 import axios from "axios";
 import { getIdToken } from "./auth/token";
 
@@ -6,14 +5,16 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE,
 });
 
-// Async interceptor that waits for the token
-api.interceptors.request.use(async (config) => {
-  const token = await getIdToken();
-  if (token) {
-    config.headers = config.headers || {};
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getIdToken();         
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
 
 export default api;
