4ea1b9e7bc7aa6813f169d343fd938e063aaad63
Li <shanlichin2021@fau.edu>
2025-04-19 22:48:47 -0400

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // tell Vite that in production all assets are served from /rtk-frontend/
